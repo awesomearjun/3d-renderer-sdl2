866c09207e5c46580ee9852aa0f696eda2668a8a
awesomearjun <iamawesomearjun@gmail.com>
2025-09-27 12:16:31 -0400

    add boilerplate project

---
+++ include/screen.hpp
@@ -0,0 +1,9 @@
+#include "SDL_events.h"
+#include "SDL_video.h"
+#include <vector>
+
+class Screen
+{
+    SDL_Event e;
+    SDL_Window *window;
+};
---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "screen.hpp"
+
+int main()
+{
+	std::cout << "hi" << std::endl;
+}
